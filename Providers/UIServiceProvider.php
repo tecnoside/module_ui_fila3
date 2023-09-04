8f2ebff82a481ad993788bf9e6b3233a97872014
marco76tv <marco.sottana@gmail.com>
2023-09-04 16:18:36 +0200

    up

---
+++ Providers/UIServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\Providers;
+
+use Modules\Xot\Providers\XotBaseServiceProvider;
+
+/**
+ * ---.
+ */
+class UIServiceProvider extends XotBaseServiceProvider
+{
+    public string $module_name = 'setting';
+
+    protected string $module_dir = __DIR__;
+
+    protected string $module_ns = __NAMESPACE__;
+}
