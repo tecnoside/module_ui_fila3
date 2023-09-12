0ccd60fa5d15c400821945c1268faeb655faaeb6
marco76tv <marco.sottana@gmail.com>
2023-09-12 10:16:01 +0200

    up

--- Filament/Pages/Dashboard.php
+++ Filament/Pages/Dashboard.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Modules\UI\Filament\Pages;
 
 use Filament\Pages\Page;
-use Savannabits\FilamentModules\Concerns\ContextualPage;
+
 
 class Dashboard extends Page
 {
-    //use ContextualPage;
+    //
     protected static ?string $navigationIcon = 'heroicon-o-home';
 
     protected static string $view = 'ui::filament.pages.dashboard';
