0e97e707d4900b823dc4088e06acb58ea19c82fb
marco76tv <marco.sottana@gmail.com>
2023-09-15 15:53:09 +0200

    up

--- Filament/Pages/Dashboard.php
+++ Filament/Pages/Dashboard.php
@@ -6,10 +6,8 @@ namespace Modules\UI\Filament\Pages;
 
 use Filament\Pages\Page;
 
-
 class Dashboard extends Page
 {
-    //
     protected static ?string $navigationIcon = 'heroicon-o-home';
 
     protected static string $view = 'ui::filament.pages.dashboard';
