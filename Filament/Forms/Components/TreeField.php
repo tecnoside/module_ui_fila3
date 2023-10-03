e35c8e56a81ad643ae6939f2b3310fda26c14351
marco76tv <marco.sottana@gmail.com>
2023-10-03 17:53:29 +0200

    up

---
+++ Filament/Forms/Components/TreeField.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\UI\Filament\Forms\Components;
+
+use Filament\Forms\Components\Field;
+
+class TreeField extends Field
+{
+    protected string $view = 'ui::filament.forms.components.tree';
+}
---
+++ Filament/Tables/Columns/TreeColumn.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\UI\Filament\Tables\Columns;
+
+use Filament\Tables\Columns\Column;
+
+class TreeColumn extends Column
+{
+    protected string $view = 'ui::filament.tables.columns.tree';
+}
