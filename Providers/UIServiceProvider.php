689404b30a152e0ddb9e21a9dbaee0789910e1ff
marco76tv <marco.sottana@gmail.com>
2023-12-20 16:04:22 +0100

    first

---
+++ Providers/UIServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\Providers;
+
+use Illuminate\Foundation\AliasLoader;
+use Modules\UI\Services\UIService;
+use Modules\Xot\Providers\XotBaseServiceProvider;
+
+/**
+ * ---.
+ */
+class UIServiceProvider extends XotBaseServiceProvider
+{
+    public string $module_name = 'ui';
+
+    protected string $module_dir = __DIR__;
+
+    protected string $module_ns = __NAMESPACE__;
+
+    /**
+     * Undocumented function.
+     */
+    public function bootCallback(): void
+    {
+        // -------
+    }
+
+    public function registerCallback(): void
+    {
+        $loader = AliasLoader::getInstance();
+        $loader->alias('ui', UIService::class);
+    }
+}
---
+++ Resources/views/filament/tables/columns/group.blade.php
@@ -0,0 +1,27 @@
+@php
+    $fields=$getFields();
+    $record=$getRecord();
+@endphp
+<div
+    {{
+        $attributes
+            ->merge($getExtraAttributes(), escape: false)
+            ->class([
+                'fi-ta-icon flex flex-wrap gap-1.5',
+                'px-3 py-4' => ! $isInline(),
+                //'flex-col' => $isListWithLineBreaks(),
+                'flex-col' => true,
+            ])
+    }}
+>
+    @foreach ($fields as $field)
+
+        {!! str_replace(', ',',<br/>',$field->record($record)->getState()) !!}
+        <br/>
+        {{--
+        {{ $field->record($record) }}
+        --}}
+    @endforeach
+
+
+</div>
