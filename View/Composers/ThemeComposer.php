411998bbf9937d8db99ea9528a7e0afcfa0d5afa
marco76tv <marco.sottana@gmail.com>
2023-12-27 15:47:31 +0100

    up

---
+++ View/Composers/ThemeComposer.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\View\Composers;
+
+class ThemeComposer
+{
+    public function metatags()
+    {
+        return '';
+    }
+
+    public function metatag()
+    {
+        return '';
+    }
+
+    public function showScripts()
+    {
+        return '';
+    }
+}
