0c7c45a43d7c1358fb48444b90a26f6c4bbb6147
marco76tv <marco.sottana@gmail.com>
2024-01-17 16:32:58 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 return [
     'name' => 'UI',
     'navigation_sort' => 1,
-<<<<<<< HEAD
     // 'icon' => 'heroicon-o-cog', // icon on dashboard
     'icon' => 'fas-theater-masks',
-=======
-    'icon' => 'heroicon-o-cog', // icon on dashboard
->>>>>>> 70f5bf9 (Rename Filament/Blocks/PageCard.php and Filament/Blocks/PostCard.php to Filament/Blocks/PageCard.to_blog and Filament/Blocks/PostCard.to_blog)
 ];
