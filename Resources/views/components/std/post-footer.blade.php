453bec8d6824e06a3fe4be7f50511125bfe4bbf8
marco76tv <marco.sottana@gmail.com>
2024-01-19 10:12:57 +0100

    rebase 6

--- Resources/views/components/std/post-footer.blade.php
+++ Resources/views/components/std/post-footer.blade.php
@@ -5,7 +5,11 @@
         <h2>See also</h2>
 
         <div class="grid gap-4 grid-cols-1 sm:grid-cols-2">
+<<<<<<< HEAD
             {{-- OBSOLETE
+=======
+            {{-- OBSOLETE 
+>>>>>>> 922b101 (up)
             <x-render-blocks :blocks="$blocks" />
             --}}
         </div>
--- Resources/views/components/std/render-blocks.blade.php
+++ Resources/views/components/std/render-blocks.blade.php
@@ -1,7 +1,11 @@
 @props(['blocks'])
 
 @foreach ($blocks as $block)
+<<<<<<< HEAD
     {{-- OBSOLETE
+=======
+    {{-- OBSOLETE 
+>>>>>>> 922b101 (up)
     <x-render-block :block="$block" />
     --}}
 @endforeach
