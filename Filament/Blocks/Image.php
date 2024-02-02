3ff731f3316d33a8357b7b6600eb73a92233ad57
marco76tv <marco76tv@users.noreply.github.com>
2024-02-02 07:37:18 +0000

    Lint

--- Filament/Blocks/Image.php
+++ Filament/Blocks/Image.php
@@ -33,7 +33,7 @@ class Image
                 TextInput::make('caption')
                     ->columnSpanFull(),
             ])
-            ->columns($context === 'form' ? 2 : 1);
+            ->columns('form' === $context ? 2 : 1);
     }
 
     public static function getRatios(): array
