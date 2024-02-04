e2269f60f6c54e7a619877564033c89617c0040d
marco76tv <marco76tv@users.noreply.github.com>
2024-02-04 07:31:04 +0000

    Lint

--- Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
+++ Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
@@ -221,7 +221,7 @@ trait HandlesTreeBuilder
                     }
                 )
                 ->requiresConfirmation()
-                ->visible($this->mountedItem != null),
+                ->visible(null != $this->mountedItem),
             Action::make('item')
                 ->mountUsing(
                     function (ComponentContainer $form): void {
