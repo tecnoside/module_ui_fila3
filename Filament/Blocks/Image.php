ce90aa42e1e758a511b798286025059bbf447ce0
marco76tv <marco.sottana@gmail.com>
2024-02-05 14:49:35 +0100

    Lint code and fix issues

--- Filament/Blocks/Image.php
+++ Filament/Blocks/Image.php
@@ -31,16 +31,10 @@ class Image
                     TextInput::make('alt')
                         ->columnSpanFull(),
 
-<<<<<<< HEAD
                     TextInput::make('caption')
                         ->columnSpanFull(),
                 ]
             )
-=======
-                TextInput::make('caption')
-                    ->columnSpanFull(),
-            ])
->>>>>>> 3ff731f (Lint)
             ->columns('form' === $context ? 2 : 1);
     }
 
