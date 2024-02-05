0cc5a3027f4651d129a488075ec192682511e9d6
marco76tv <marco.sottana@gmail.com>
2024-02-05 14:51:13 +0100

    Update Filament block classes and admin panel provider configuration

--- Filament/Blocks/Title.php
+++ Filament/Blocks/Title.php
@@ -17,7 +17,6 @@ class Title
         return Block::make($name)
             ->schema(
                 [
-<<<<<<< HEAD
                     TextInput::make('text')
                         ->required(),
 
@@ -30,20 +29,6 @@ class Title
                             ]
                         )
                         ->afterStateHydrated(static fn ($state, $set) => $state || $set('level', 'h2')),
-=======
-                TextInput::make('text')
-                    ->required(),
-
-                Select::make('level')
-                    ->options(
-                        [
-                        'h2' => 'h2',
-                        'h3' => 'h3',
-                        'h4' => 'h4',
-                        ]
-                    )
-                    ->afterStateHydrated(static fn ($state, $set) => $state || $set('level', 'h2')),
->>>>>>> 4b2b025 (up)
                 ]
             )
             ->columns('form' === $context ? 2 : 1);
