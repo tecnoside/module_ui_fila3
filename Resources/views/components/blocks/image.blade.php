2bf7122add3ce86d36d4a5715bbf6ca9441524cd
marco76tv <marco.sottana@gmail.com>
2024-02-06 09:46:22 +0100

    .

--- Resources/views/components/blocks/image.blade.php
+++ Resources/views/components/blocks/image.blade.php
@@ -11,6 +11,7 @@
     if(!$src){
         $src = $model->media->first()->getUrl();
     }
+    dddx($src);
     $ratioClass = \Modules\UI\Filament\Blocks\Image::getRatioClass($ratio ?: '4-3');
 @endphp
 
