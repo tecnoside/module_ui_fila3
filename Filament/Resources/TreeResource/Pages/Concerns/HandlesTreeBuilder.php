c298a3d959d1d4dbbe5a08f62b7be0413ca247b1
marco76tv <marco.sottana@gmail.com>
2024-02-08 10:36:37 +0100

    rebase 6/11

--- Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
+++ Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
@@ -222,7 +222,7 @@ trait HandlesTreeBuilder
                     }
                 )
                 ->requiresConfirmation()
-                ->visible($this->mountedItem != null),
+                ->visible(null != $this->mountedItem),
             Action::make('item')
                 ->mountUsing(
                     function (ComponentContainer $form): void {
