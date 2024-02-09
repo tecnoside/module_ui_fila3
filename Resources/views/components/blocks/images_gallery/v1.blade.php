c171fc18ea66561e327856845bff476bdb46bd07
marco76tv <marco.sottana@gmail.com>
2024-02-09 11:58:51 +0100

    wip blocks\images_gallery\v1.blade.php

--- Resources/views/components/blocks/images_gallery/v1.blade.php
+++ Resources/views/components/blocks/images_gallery/v1.blade.php
@@ -1,14 +1,18 @@
 <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-wrap">
-      <div class="flex w-full mb-20 flex-wrap">
-        @if($data['title'])
-          <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">{{ $data['title'] }}</h1>
-        @endif
-        @if($data['subtitle'])
-          <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">{{ $data['subtitle'] }}</p>
-        @endif
-      </div>
+      @if($data['title'])
+        <div class="flex w-full mb-20 flex-wrap">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">{{ $data['title'] }}</h1>
+            @if($data['subtitle'])
+              <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">{{ $data['subtitle'] }}</p>
+            @endif
+        </div>
+      @endif
       <div class="flex flex-wrap md:-m-2 -m-1">
+        {{-- {{ dddx([
+          $model->content_blocks,
+          $model->getImagesGallery()
+          ]) }} --}}
         <div class="flex flex-wrap w-1/2">
           <div class="md:p-2 p-1 w-1/2">
             <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300">
