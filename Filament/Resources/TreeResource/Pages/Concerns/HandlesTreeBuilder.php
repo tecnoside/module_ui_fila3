7ea08d4faaec5bb7db062b83e6d95f71f5f97d80
marco76tv <marco76tv@users.noreply.github.com>
2024-02-15 07:24:34 +0000

    Lint

--- Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
+++ Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
@@ -129,7 +129,7 @@ trait HandlesTreeBuilder
         $up = tap($row)->update($data);
         Assert::isArray($item = data_get($this, $this->mountedItem));
         $up = array_merge($item, $up->toArray());
-        if ($this->mountedItem === null) {
+        if (null === $this->mountedItem) {
             return;
         }
         data_set($this, $this->mountedItem, $up);
@@ -169,7 +169,7 @@ trait HandlesTreeBuilder
     {
         $model = $this->getResource()::getModel();
         $data['parent_id'] = $record?->getKey();
-        if ($record === null) {
+        if (null === $record) {
             return;
         }
         Assert::string($parent_id = $data['parent_id']);
@@ -214,7 +214,7 @@ trait HandlesTreeBuilder
                     }
                 )
                 ->requiresConfirmation()
-                ->visible($this->mountedItem !== null),
+                ->visible(null !== $this->mountedItem),
             Action::make('item')
                 ->mountUsing(
                     function (ComponentContainer $form): void {
