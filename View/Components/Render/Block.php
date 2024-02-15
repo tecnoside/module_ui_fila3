1c20c2ea9b3bb9db762cd5a75a2e90318a988f40
marco76tv <marco.sottana@gmail.com>
2024-02-15 16:49:25 +0100

    Fix block view not found exception

--- View/Components/Render/Block.php
+++ View/Components/Render/Block.php
@@ -32,7 +32,7 @@ class Block extends Component
         }
 
         $views = ['ui::components.blocks.'.$this->tpl];
-        if ($this->model !== null) {
+        if (null !== $this->model) {
             $module = app(GetModuleNameFromModelAction::class)->execute($this->model);
             $views[] = strtolower($module).'::components.blocks.'.$this->tpl;
         }
@@ -41,7 +41,7 @@ class Block extends Component
          * @phpstan-var view-string|null
          */
         $view = Arr::first($views, static fn (string $view) => view()->exists($view));
-        if ($view === null) {
+        if (null === $view) {
             throw new \Exception('none of these views exists ['.implode(', '.chr(13), $views).']');
         }
         $view_params = $this->block['data'] ?? [];
