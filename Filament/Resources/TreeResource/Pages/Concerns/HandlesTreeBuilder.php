e1284b815c7afb81b66980387fee0b4b82437a65
marco76tv <marco76tv@users.noreply.github.com>
2024-02-15 15:51:30 +0000

    Dusting

--- Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
+++ Filament/Resources/TreeResource/Pages/Concerns/HandlesTreeBuilder.php
@@ -132,7 +132,7 @@ trait HandlesTreeBuilder
         $up = tap($row)->update($data);
         Assert::isArray($item = data_get($this, $this->mountedItem));
         $up = array_merge($item, $up->toArray());
-        if (null == $this->mountedItem) {
+        if ($this->mountedItem == null) {
             return;
         }
         data_set($this, $this->mountedItem, $up);
@@ -170,7 +170,7 @@ trait HandlesTreeBuilder
 
     public function storeItem(?Model $record, array $data): void
     {
-        if (null === $record) {
+        if ($record === null) {
             return;
         }
 
@@ -220,7 +220,7 @@ trait HandlesTreeBuilder
                     }
                 )
                 ->requiresConfirmation()
-                ->visible(null !== $this->mountedItem),
+                ->visible($this->mountedItem !== null),
             Action::make('item')
                 ->mountUsing(
                     function (ComponentContainer $form): void {
--- View/Components/Render/Block.php
+++ View/Components/Render/Block.php
@@ -25,14 +25,14 @@ class Block extends Component
 
     public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\View\View
     {
-        if ('v1' == $this->tpl) {
+        if ($this->tpl == 'v1') {
             $this->tpl = $this->block['type'];
         } else {
             $this->tpl = $this->block['type'].'.'.$this->tpl;
         }
 
         $views = ['ui::components.blocks.'.$this->tpl];
-        if (null !== $this->model) {
+        if ($this->model !== null) {
             $module = app(GetModuleNameFromModelAction::class)->execute($this->model);
             $views[] = strtolower($module).'::components.blocks.'.$this->tpl;
         }
@@ -41,7 +41,7 @@ class Block extends Component
          * @phpstan-var view-string|null
          */
         $view = Arr::first($views, static fn (string $view) => view()->exists($view));
-        if (null === $view) {
+        if ($view === null) {
             throw new \Exception('none of these views exists ['.implode(', '.chr(13), $views).']');
         }
         $view_params = $this->block['data'] ?? [];
