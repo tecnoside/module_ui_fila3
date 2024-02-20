87e37a63ac420b9962f17faf5b428c31417bc690
marco76tv <marco76tv@users.noreply.github.com>
2024-02-20 17:36:23 +0000

    Lint

--- Http/Livewire/Toast.php
+++ Http/Livewire/Toast.php
@@ -1,24 +1,24 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @see https://github.com/bezhanSalleh/filament-language-switch/blob/main/src/Http/Livewire/FilamentLanguageSwitch.php
  */
 
 namespace BezhanSalleh\FilamentLanguageSwitch\Http\Livewire;
 
-use BezhanSalleh\FilamentLanguageSwitch\Events\LocaleChanged;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Toast extends Component
 {
-
-
     public function render(): View
     {
         $view = 'ui::livewire.toast';
         $view_params = [
-            'view'=>$view,
+            'view' => $view,
         ];
-        return view($view,$view_params);
+
+        return view($view, $view_params);
     }
 }
