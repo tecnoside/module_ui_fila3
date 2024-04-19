80993bac3dccbabd954f675429a25869eb58a61b
Malebestia <vair81@gmail.com>
2024-04-19 17:10:04 +0200

    .

--- Resources/views/components/buttons/go-home.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['href'])
-<x-filament::button
-    href="{{ $href }}"
-    tag="a"
-    color="info"
-    icon="heroicon-o-home"
-
-/>
-
--- Resources/views/filament/widgets/children.blade.php
+++
@@ -1,4 +0,0 @@
-<x-filament::widget class="filament-widgets-tree-widget">
-    {{-- $this->tree --}}
-    {{ dddx($record) }}
-</x-filament::widget>
