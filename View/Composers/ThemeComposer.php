6fd28934563ad038e1694064f70dc95f46daf9f2
Malebestia <vair81@gmail.com>
2024-05-07 17:19:45 +0200

    wip blade metatag

--- View/Composers/ThemeComposer.php
+++ View/Composers/ThemeComposer.php
@@ -6,15 +6,34 @@ namespace Modules\UI\View\Composers;
 
 class ThemeComposer
 {
-    public function metatags(): string
-    {
-        return '';
+    /**
+     * @return string
+     */
+    public function metatags() {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = 'ui::metatags';
+
+        return (string) view($view)->render();
     }
 
-    public function metatag(): string
-    {
-        return '';
+    /**
+     * @param string $index
+     *
+     * @return \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|mixed
+     */
+    public function metatag($index) {
+        $ris = self::__getStatic($index);
+        // echo '<br/>['.$index.']['.$ris.']';
+        if ('' === $ris || null === $ris) {
+            $ris = config('metatag.'.$index);
+            self::__setStatic($index, $ris);
+        }
+
+        return $ris;
     }
+
 
     public function showScripts(): string
     {
