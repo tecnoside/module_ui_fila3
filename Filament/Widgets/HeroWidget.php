01a511bbedcdba7194d2ec6cf6ccc97ab147c46f
marco76tv <marco.sottana@gmail.com>
2024-05-15 19:57:24 +0200

    up

---
+++ Filament/Widgets/HeroWidget.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\Filament\Widgets;
+
+use Carbon\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
+
+class HeroWidget extends BaseWidget
+{
+    //use InteractsWithPageFilters;
+
+    //protected static ?int $sort = 0;
+
+    public string $title='no-set';
+
+    protected function getStats(): array
+    {
+        
+        return [
+            Stat::make('', $this->title)
+                //->description('32k increase '.$startDate)
+                //->descriptionIcon('heroicon-m-arrow-trending-up')
+                //->chart([7, 2, 10, 3, 15, 4, 17])
+                //->color('success')
+                //->icon('heroicon-s-fire')
+                ,
+        ];
+    }
+
+    public function getColumns(): int
+    {
+        return 8;
+    }
+}
