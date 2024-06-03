0bacaa7e6cebe3f9aa2a58dd9e7f6607532003ad
marco76tv <marco76tv@users.noreply.github.com>
2024-06-03 20:29:58 +0000

    Lint

--- Filament/Widgets/OverlookWidget.php
+++ Filament/Widgets/OverlookWidget.php
@@ -1,39 +1,35 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @see https://github.com/awcodes/overlook/blob/2.x/src/Widgets/OverlookWidget.php
  */
 
 namespace Modules\UI\Filament\Widgets;
 
-use Exception;
-use NumberFormatter;
 use Filament\Widgets\Widget;
-use Awcodes\Overlook\OverlookPlugin;
-use Modules\Quaeris\Actions\Dashboard\StatsAction;
-use Awcodes\Overlook\Contracts\CustomizeOverlookWidget;
 
 class OverlookWidget extends Widget
 {
     protected static string $view = 'ui::filament.widgets.overlook';
 
-    protected int | string | array $columnSpan = 1;
+    protected int|string|array $columnSpan = 1;
 
-    public string $icon='heroicon-o-envelope';
-    public string $title='';
+    public string $icon = 'heroicon-o-envelope';
+    public string $title = '';
 
+    /*
+        public array $grid = [
+                    'default' => 6,
+                    'sm' => 6,
+                    'md' => 6,
+                    'lg' => 6,
+                    'xl' => 6,
+                    '2xl' => null,
+                ];
+                */
 
-/*
-    public array $grid = [
-                'default' => 6,
-                'sm' => 6,
-                'md' => 6,
-                'lg' => 6,
-                'xl' => 6,
-                '2xl' => null,
-            ];
-            */
-
-    public array $stats=[];
+    public array $stats = [];
 
     /*
     public function mount(array $filter): void
@@ -54,6 +50,4 @@ class OverlookWidget extends Widget
         }
     }
     */
-
-
 }
--- Filament/Widgets/RowWidget.php
+++ Filament/Widgets/RowWidget.php
@@ -1,27 +1,21 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @see https://github.com/awcodes/overlook/blob/2.x/src/Widgets/OverlookWidget.php
  */
 
 namespace Modules\Quaeris\Filament\Widgets;
 
-use Exception;
-use NumberFormatter;
 use Filament\Widgets\Widget;
-use Awcodes\Overlook\OverlookPlugin;
-use Modules\Quaeris\Actions\Dashboard\StatsAction;
-use Awcodes\Overlook\Contracts\CustomizeOverlookWidget;
 
 class RowWidget extends Widget
 {
     protected static string $view = 'ui::filament.widgets.row';
 
-    protected int | string | array $columnSpan = 'full';
+    protected int|string|array $columnSpan = 'full';
 
     public array $grid = [];
 
     public array $widgets = [];
-
-
-
 }
