5d25d302b35fee1f3bf17229a6e9b1cc750363b1
marco <marco.sottana@gmail.com>
2024-06-04 14:54:21 +0200

    🔧 (HeroWidget.php): Remove unnecessary code and conflicts from HeroWidget.php file to clean up the codebase and improve readability.

--- Filament/Widgets/HeroWidget.php
+++ Filament/Widgets/HeroWidget.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -31,42 +29,12 @@ class HeroWidget extends BaseWidget
                 // ->chart([7, 2, 10, 3, 15, 4, 17])
                 // ->color('success')
                 ->icon($this->icon),
-=======
-use Carbon\Carbon;
-=======
->>>>>>> 700c8d5 (Lint)
-use Filament\Widgets\Concerns\InteractsWithPageFilters;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class HeroWidget extends BaseWidget
-{
-    // use InteractsWithPageFilters;
-
-    // protected static ?int $sort = 0;
-
-    public string $title = 'no-set';
-
-    protected function getStats(): array
-    {
-        return [
-            Stat::make('', $this->title)
-<<<<<<< HEAD
-                //->description('32k increase '.$startDate)
-                //->descriptionIcon('heroicon-m-arrow-trending-up')
-                //->chart([7, 2, 10, 3, 15, 4, 17])
-                //->color('success')
-                //->icon('heroicon-s-fire')
-                ,
->>>>>>> 01a511b (up)
-=======
             // ->description('32k increase '.$startDate)
             // ->descriptionIcon('heroicon-m-arrow-trending-up')
             // ->chart([7, 2, 10, 3, 15, 4, 17])
             // ->color('success')
             // ->icon('heroicon-s-fire')
-            ,
->>>>>>> 700c8d5 (Lint)
         ];
     }
 
@@ -74,12 +42,4 @@ class HeroWidget extends BaseWidget
     {
         return 8;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 01a511b (up)
-=======
-}
->>>>>>> 700c8d5 (Lint)
