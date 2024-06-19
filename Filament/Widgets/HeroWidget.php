0dab5afc9913ecfd62d6eea102f7db746938bab6
marco <marco.sottana@gmail.com>
2024-06-19 18:23:11 +0200

    📝 (HeroWidget.php): Add Carbon and Number classes for better date handling and number formatting ♻️ (HeroWidget.php): Refactor code to improve readability and remove commented out code ✨ (HeroWidget.php): Add support for displaying an icon in the HeroWidget and set default values for title and icon properties

--- Filament/Widgets/HeroWidget.php
+++ Filament/Widgets/HeroWidget.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
+use Carbon\Carbon;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class HeroWidget extends BaseWidget
 {
-    // use InteractsWithPageFilters;
+    //use InteractsWithPageFilters;
 
-    // protected static ?int $sort = 0;
+    //protected static ?int $sort = 0;
 
-    public string $title = 'no-set';
+    public string $title='no-set';
+    public string $icon='';
 
     public string $icon = '';
 
@@ -22,9 +25,19 @@ class HeroWidget extends BaseWidget
 
     protected function getStats(): array
     {
+        
         return [
             Stat::make('', $this->title)
+<<<<<<< HEAD
                 ->icon($this->icon),
+=======
+                //->description('32k increase '.$startDate)
+                //->descriptionIcon('heroicon-m-arrow-trending-up')
+                //->chart([7, 2, 10, 3, 15, 4, 17])
+                //->color('success')
+                ->icon($this->icon)
+                ,
+>>>>>>> da7aac2 (up)
         ];
     }
 
