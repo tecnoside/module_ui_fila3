febcff80ce1a7daab1ea331bbb64ed51f0f69a79
marco <marco.sottana@gmail.com>
2024-06-19 18:21:45 +0200

    📝 (HeroWidget.php): Remove unnecessary code and resolve conflicts in HeroWidget.php file.

--- Filament/Widgets/HeroWidget.php
+++ Filament/Widgets/HeroWidget.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
+<<<<<<< HEAD
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -25,6 +26,33 @@ class HeroWidget extends BaseWidget
         return [
             Stat::make('', $this->title)
                 ->icon($this->icon),
+=======
+use Carbon\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
+
+class HeroWidget extends BaseWidget
+{
+    //use InteractsWithPageFilters;
+
+    //protected static ?int $sort = 0;
+
+    public string $title='no-set';
+
+    protected function getStats(): array
+    {
+        
+        return [
+            Stat::make('', $this->title)
+                //->description('32k increase '.$startDate)
+                //->descriptionIcon('heroicon-m-arrow-trending-up')
+                //->chart([7, 2, 10, 3, 15, 4, 17])
+                //->color('success')
+                //->icon('heroicon-s-fire')
+                ,
+>>>>>>> 01a511b (up)
         ];
     }
 
@@ -32,4 +60,8 @@ class HeroWidget extends BaseWidget
     {
         return 8;
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 01a511b (up)
