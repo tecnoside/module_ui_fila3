6c323c5f343932fecef128ae40489a5abb2bd742
marco <marco.sottana@gmail.com>
2024-06-19 18:22:06 +0200

    🔧 (HeroWidget.php): resolve conflict in HeroWidget.php file related to changes in title and icon properties 📝 (HeroWidget.php): update getStats method to include icon property in the returned array

--- Filament/Widgets/HeroWidget.php
+++ Filament/Widgets/HeroWidget.php
@@ -15,16 +15,28 @@ class HeroWidget extends BaseWidget
     // protected static ?int $sort = 0;
 
     public string $title = 'no-set';
+<<<<<<< HEAD
 
     public string $icon = '';
 
     protected static ?string $pollingInterval = null;
+=======
+>>>>>>> 700c8d5 (Lint)
 
     protected function getStats(): array
     {
         return [
             Stat::make('', $this->title)
+<<<<<<< HEAD
                 ->icon($this->icon),
+=======
+            // ->description('32k increase '.$startDate)
+            // ->descriptionIcon('heroicon-m-arrow-trending-up')
+            // ->chart([7, 2, 10, 3, 15, 4, 17])
+            // ->color('success')
+            // ->icon('heroicon-s-fire')
+            ,
+>>>>>>> 700c8d5 (Lint)
         ];
     }
 
