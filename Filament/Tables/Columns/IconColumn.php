f19bfe26bfe6b1b1499d3cd7b7eb359b9c9ddabb
marco76tv <marco.sottana@gmail.com>
2024-06-30 19:32:47 +0200

    .

---
+++ Filament/Tables/Columns/IconColumn.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\UI\Filament\Tables\Columns;
+
+use Filament\Tables\Columns\Column;
+
+class IconColumn extends Column
+{
+
+    protected string $view = 'ui::filament.tables.columns.icon';
+
+}
