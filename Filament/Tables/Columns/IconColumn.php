4b00f250faab32a3f88073fe1a071b5d61069906
marco <marco.sottana@gmail.com>
2024-07-01 15:29:22 +0200

    up

--- Filament/Tables/Columns/IconColumn.php
+++ Filament/Tables/Columns/IconColumn.php
@@ -1,22 +1,12 @@
 <?php
 
-<<<<<<< HEAD
 declare(strict_types=1);
 
-=======
->>>>>>> origin/dev
 namespace Modules\UI\Filament\Tables\Columns;
 
 use Filament\Tables\Columns\Column;
 
 class IconColumn extends Column
 {
-<<<<<<< HEAD
     protected string $view = 'ui::filament.tables.columns.icon';
 }
-=======
-
-    protected string $view = 'ui::filament.tables.columns.icon';
-
-}
->>>>>>> origin/dev
