af458f306f06b1875a8c6802737cc76ce09ecfaa
marco76tv <marco.sottana@gmail.com>
2024-07-01 20:30:44 +0200

    feat(Block.php): add support for fetching all blocks and finding a specific block by name refactor(Block.php): simplify module name retrieval and view existence check logic in Block component

--- View/Components/Render/Block.php
+++ View/Components/Render/Block.php
@@ -7,8 +7,10 @@ namespace Modules\UI\View\Components\Render;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 use Illuminate\View\View;
+use Modules\Blog\Actions\Block\GetAllBlocksAction;
 use Modules\Xot\Actions\Module\GetModuleNameByModelAction;
 
 /**
@@ -38,23 +40,37 @@ class Block extends Component
         } else {
             $this->tpl = $this->block['type'].'.'.$this->tpl;
         }
+        $blocks = app(GetAllBlocksAction::class)->execute();
+        $block = Arr::first($blocks, function ($block) {
+            return $block['name'] === $this->block['type'];
+        });
+        $module = Arr::get($block, 'module', 'UI');
+        $module_low = Str::lower($module);
+        // $backtrace = debug_backtrace();
 
-        $views = ['ui::components.blocks.'.$this->tpl];
-        if (null !== $this->model) {
-            $module = app(GetModuleNameByModelAction::class)->execute($this->model);
-            $views[] = strtolower($module).'::components.blocks.'.$this->tpl;
-        }
+        // $views = ['ui::components.blocks.'.$this->tpl];
+        // if (null !== $this->model) {
+        //    $module = app(GetModuleNameByModelAction::class)->execute($this->model);
+        //    $views[] = strtolower($module).'::components.blocks.'.$this->tpl;
+        // }
 
-        /**
+        /*
          * @callable
          */
-        $callback = static fn (string $view) => view()->exists($view);
+        // $callback = static fn (string $view) => view()->exists($view);
+        /*
+         * @phpstan-var view-string|null
+         */
+        // $view = Arr::first($views, $callback);
+        // if (null === $view) {
+        //    throw new \Exception('none of these views exists ['.implode(', '.\chr(13), $views).']');
+        // }
         /**
          * @phpstan-var view-string|null
          */
-        $view = Arr::first($views, $callback);
-        if (null === $view) {
-            throw new \Exception('none of these views exists ['.implode(', '.\chr(13), $views).']');
+        $view = $module_low.'::components.blocks.'.$this->tpl;
+        if (! view()->exists($view)) {
+            throw new \Exception('view not exitst ['.$view.']');
         }
         $view_params = $this->block['data'] ?? [];
 
