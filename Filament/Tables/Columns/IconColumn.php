226de40561b34c0d21c2a7c732b388a3c93f9ce6
Malebestia <vair81@gmail.com>
2024-07-03 08:59:15 +0200

    conflict

--- Filament/Tables/Columns/IconColumn.php
+++ Filament/Tables/Columns/IconColumn.php
@@ -6,12 +6,5 @@ use Filament\Tables\Columns\Column;
 
 class IconColumn extends Column
 {
-<<<<<<< HEAD
     protected string $view = 'ui::filament.tables.columns.icon';
 }
-=======
-
-    protected string $view = 'ui::filament.tables.columns.icon';
-
-}
->>>>>>> origin/dev
