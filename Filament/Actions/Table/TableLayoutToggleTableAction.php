93ad30f1b1a67d6ef45cb9767bb119588f00cd21
marco <marco.sottana@gmail.com>
2024-07-30 18:34:19 +0200

    up

---
+++ Filament/Actions/Table/TableLayoutToggleTableAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\Filament\Actions\Table;
+
+use Filament\Tables\Actions\Action;
+
+/**
+ * @see https://filamentphp.com/plugins/tgeorgel-table-layout-toggle
+ */
+class TableLayoutToggleTableAction extends Action
+{
+    // use NavigationActionLabelTrait;
+    public string $list_icon = 'heroicon-o-list-bullet';
+    public string $grid_icon = 'heroicon-o-squares-2x2';
+
+    public static function getDefaultName(): ?string
+    {
+        return 'table-layout-toggle-header';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->translateLabel()
+            ->color('secondary')
+            ->label('')
+            // ->label(trans('ui::'.static::getDefaultName().'.label'))
+            // ->tooltip(trans('setting::database_connection.actions.database-backup.tooltip'))
+            // ->icon(trans('setting::database_connection.actions.database-backup.icon'))
+            // ->icon($this->list_icon)
+            ->icon(fn ($livewire) => 'list' == $livewire->layoutView ? $this->list_icon : $this->grid_icon)
+            ->action(
+                function ($livewire) {
+                    $livewire->layoutView = ('grid' == $livewire->layoutView ? 'list' : 'grid');
+                }
+            );
+    }
+}
