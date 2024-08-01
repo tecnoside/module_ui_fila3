dc6b635095e8ad53285db548dca0fd6becf2125f
marco <marco.sottana@gmail.com>
2024-08-01 12:27:49 +0200

    up

--- Filament/Actions/Table/TableLayoutToggleTableAction.php
+++ Filament/Actions/Table/TableLayoutToggleTableAction.php
@@ -12,8 +12,6 @@ use Filament\Tables\Actions\Action;
 class TableLayoutToggleTableAction extends Action
 {
     // use NavigationActionLabelTrait;
-    public string $list_icon = 'heroicon-o-list-bullet';
-    public string $grid_icon = 'heroicon-o-squares-2x2';
 
     public static function getDefaultName(): ?string
     {
@@ -24,16 +22,19 @@ class TableLayoutToggleTableAction extends Action
     {
         parent::setUp();
         $this->translateLabel()
-            ->color('secondary')
+            ->color('gray')
             ->label('')
             // ->label(trans('ui::'.static::getDefaultName().'.label'))
             // ->tooltip(trans('setting::database_connection.actions.database-backup.tooltip'))
-            // ->icon(trans('setting::database_connection.actions.database-backup.icon'))
-            // ->icon($this->list_icon)
-            ->icon(fn ($livewire) => 'list' == $livewire->layoutView ? $this->list_icon : $this->grid_icon)
+            ->icon(fn ($livewire) => $livewire->layoutView->getIcon())
             ->action(
                 function ($livewire) {
-                    $livewire->layoutView = ('grid' == $livewire->layoutView ? 'list' : 'grid');
+                    $livewire->layoutView = $livewire->layoutView->toggle();
+
+                    // $livewire->dispatch('$refresh');
+
+                    // $livewire->dispatch('layoutViewChanged');
+                    // $livewire->tableLayoutToggle();
                 }
             );
     }
