0de83d4286436edd081d55b444296c8c97bcaca9
marco76tv <marco76tv@users.noreply.github.com>
2024-10-15 09:42:37 +0000

    Lint

--- Filament/Actions/Table/TableLayoutToggleTableAction.php
+++ Filament/Actions/Table/TableLayoutToggleTableAction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Actions\Table;
 
-use Exception;
 use Filament\Tables\Actions\Action;
 use Illuminate\Support\Facades\Session;
 use Modules\UI\Enums\TableLayoutEnum;
